6437abcd0ca31d3344c05c04aac67e6e38b89c41
Maxim Goryachkin <maxgoryachkin@mail.ru>
2021-07-20 20:51:08 +0300

    Initial Commit

---
+++ Personal Quiz/ViewControllers/QuestionViewController.swift
@@ -0,0 +1,151 @@
+//
+//  QuestionViewController.swift
+//  Personal Quiz
+//
+//  Created by brubru on 19.07.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    // MARK: - IB Outlets
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet weak var rangedSlider: UISlider! {
+        didSet {
+            let answerCount = Float(currentAnswers.count - 1)
+            
+            rangedSlider.value = answerCount / 2
+            rangedSlider.maximumValue = answerCount
+        }
+    }
+    
+    @IBOutlet weak var questionProgerssView: UIProgressView!
+    
+    // MARK: - Private properties
+    
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    private var answersChooser: [Answer] = []
+    
+    // MARK: - Override methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - IB Actions
+ 
+    @IBAction func singleAnnserButtonBressed(_ sender: UIButton) {
+        guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
+        let currentAnswer = currentAnswers[buttonIndex]
+        answersChooser.append(currentAnswer)
+        
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnserButtonBressed() {
+        for (multipleSwitch, answer) in zip(multipleSwitches, currentAnswers) {
+            if multipleSwitch.isOn {
+                answersChooser.append(answer)
+            }
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        let index = lrintf(rangedSlider.value)
+        answersChooser.append(currentAnswers[index])
+        
+        nextQuestion()
+    }
+}
+
+// MARK: - Private Methods
+
+extension QuestionViewController {
+    private func setupUI() {
+        for stackView in [singleStackView, multipleStackView, rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        let currentQuestion = questions[questionIndex]
+        questionLabel.text = currentQuestion.title
+        
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        questionProgerssView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+        case .multiple: showMultipleStackView(with: currentAnswers)
+        case .ranged: showRangedStackView(with: currentAnswers)
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden = false
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    }
+    
+    private func showMultipleStackView(with answers: [Answer]) {
+        multipleStackView.isHidden = false
+        
+        for selector in multipleSwitches {
+            selector.isOn = false
+        }
+        
+        for (label, answer) in zip(multipleLabels, answers) {
+            label.text = answer.title
+        }
+    }
+    
+    private func showRangedStackView(with answers: [Answer]) {
+        rangedStackView.isHidden = false
+        
+        rangedLabels.first?.text = answers.first?.title
+        rangedLabels.last?.text = answers.last?.title
+    }
+    
+    private func nextQuestion() {
+        questionIndex += 1
+        
+        if questionIndex < questions.count {
+            setupUI()
+            return
+        }
+        
+        performSegue(withIdentifier: "resultSegue", sender: nil)
+    }
+    
+    // Передаем массив с ответами
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultVC = segue.destination as? ResultsViewController else { return }
+        resultVC.answers = answersChooser
+    }
+}
---
+++ Personal Quiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ResultsViewController.swift
+//  Personal Quiz
+//
+//  Created by brubru on 19.07.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var navigationProperty: UINavigationItem!
+    
+    // MARK: - Public properties
+    
+    var answers: [Answer]! = [] // Передаем массив с ответами
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встерчающийся тип живтоного
+    // 3. Отобразить результат в соответсвии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экране результатов
+    
+    // MARK: - Override methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Скрываем кнопку "Back"
+        navigationItem.hidesBackButton = true
+        
+        let answer = sorting()
+        
+        //Отображаем результат в соответсвии с этим животным
+        descriptionLabel.text = answer.type.definition
+        emojiLabel.text = "Вы - \(answer.type.rawValue)"
+    }
+    
+    // MARK: - Private methods
+    
+    // Определяем наиболее часто встерчающийся тип живтоного
+    private func sorting() -> Answer {
+        let sortedSet = NSCountedSet(array: answers)
+        let mostFrequent = sortedSet.max {
+            sortedSet.count(for: $0) < sortedSet.count(for: $1)
+        }
+        let answer = mostFrequent as! Answer
+        return answer
+    }
+
+    
+    
+}
